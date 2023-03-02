58b52d42f2087221232ae87e2b19f98520f143e2
zsm4g6 <112092691+zsm4g6@users.noreply.github.com>
2023-03-02 17:31:40 -0600

    Add files via upload

--- Lab2.c
+++ Lab2.c
@@ -21,6 +21,7 @@ void parseRemainder(FILE* fp,char* inst,int* reg2,int* reg3,int* imm){
     int count=1;
     int j=11;
     int neg=0;
+    int i=0;
     *reg3=0;
     fscanf(fp,"%s",temp);
     if(strcmp(inst,"add")==0){
@@ -93,7 +94,7 @@ void parseRemainder(FILE* fp,char* inst,int* reg2,int* reg3,int* imm){
         }
         
         fscanf(fp,"%s",temp);
-        int i=0;
+        
         while(temp[i]!='\0'){
             if((int)temp[i]==45){
                 neg=-1;
@@ -119,15 +120,24 @@ void parseRemainder(FILE* fp,char* inst,int* reg2,int* reg3,int* imm){
             else{
                 j=(int)temp[i]-48;
                 if((i+neg)>0){
-                    *reg3*=10;
+                    *imm*=10;
                 }
-                 *reg3+=j;
+                 *imm+=j;
             }
             i++;
         }
         if(neg==-1){
-            *reg3*=neg;
+            *imm*=neg;
         }
+        if (temp[i+3]=='\0'){
+            *reg2=(int)temp[i+1]-48;
+        }
+        else{
+            *reg2=(int)temp[i+1]-48;
+            *reg2*=10;
+            *reg2+=(int)temp[i+2]-48;
+        }
+
     }
 
 }
@@ -140,10 +150,10 @@ int main(){
     char output[6];
     int reg1,reg2,reg3,imm;
     int *reg1x=&reg1,*reg2x=&reg2,*reg3x=&reg3,*immx=&imm;
+    
     parseFromFile(fp,output,reg1x);
-    parseRemainder(fp,output,reg2x,reg3x,imm);
-    printf("%d %d\n",reg2,reg3);
-    //printf("%s %d\n",output,reg1);
+    parseRemainder(fp,output,reg2x,reg3x,immx);
+    printf("%s %d %d %d\n",output,reg1,reg2,reg3);
     return 0;
 }
 
